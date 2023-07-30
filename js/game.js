ac09b4b696dba2531a6117dcbdb0322893986414
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2023-07-30 22:26:46 +0200

    added MovableObject

---
+++ js/game.js
@@ -0,0 +1,11 @@
+let canvas;
+let ctx; // Benennung ist Standard bei 2D-Canvas
+let character = new MovableObject();
+
+
+function init() {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
+console.log(character);
+}
