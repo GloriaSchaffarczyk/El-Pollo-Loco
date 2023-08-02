00b7a1951c49cb07a7b7437cf9baee14c1b87697
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2023-08-02 20:56:32 +0200

    added images

--- js/game.js
+++ js/game.js
@@ -1,11 +1,11 @@
 let canvas;
 let ctx; // Benennung ist Standard bei 2D-Canvas
-let character = new MovableObject();
+let world = new World();
 
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
-console.log(character);
+console.log('My character is', world['character']);
 }
