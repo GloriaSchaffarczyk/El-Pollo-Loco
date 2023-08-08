eebc9ce2dda553a64b1e7db78f5b462f657e6531
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2023-08-08 23:53:09 +0200

    added comments

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -7,11 +7,9 @@ class Character extends MovableObject {
             '../img2/2_character/1_idle/idle/biker_idle_02.png',
             '../img2/2_character/1_idle/idle/biker_idle_03.png',
             '../img2/2_character/1_idle/idle/biker_idle_04.png',
-        ]);
-
+        ]); // Bilder werden reingegeben
     }
 
     jump() {
-
     }
 }
