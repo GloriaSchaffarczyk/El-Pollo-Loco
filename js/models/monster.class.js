af712d572679e8c6168b529faabb955cdae584ee
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2023-08-11 23:30:29 +0200

    animated monsters

--- js/models/monster.class.js
+++ js/models/monster.class.js
@@ -1,10 +1,29 @@
 class Monster extends MovableObject {
+    y = 327;
+    height = 60;
+    width = 60;
+    IMAGES_WALKING = [
+        '../img2/3_enemies/monster/1_walk/monster_walk_06.png',
+        '../img2/3_enemies/monster/1_walk/monster_walk_05.png',
+        '../img2/3_enemies/monster/1_walk/monster_walk_04.png',
+        '../img2/3_enemies/monster/1_walk/monster_walk_03.png',
+        '../img2/3_enemies/monster/1_walk/monster_walk_02.png',
+        '../img2/3_enemies/monster/1_walk/monster_walk_01.png',
+    ];
+
     constructor() {
         super().loadImage('../img2/3_enemies/monster/1_walk/monster_walk_01.png');
-
+        this.loadImages(this.IMAGES_WALKING);
         this.x = 270 + Math.random() * 500;
-        this.y = 327;
-        this.height = 60;
-        this.width = 60;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++
+        }, 300);
     }
 }
