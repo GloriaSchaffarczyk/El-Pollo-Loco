cecc8f3b2e005c786ec764b75673b4b337eb9eeb
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2023-08-16 22:45:13 +0200

    tried adding direction to character

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -1,25 +1,39 @@
 class Character extends MovableObject {
+    speed = 10;
     IMAGES_IDLE = [
         '../img2/2_character/1_idle/idle/biker_idle_01.png',
         '../img2/2_character/1_idle/idle/biker_idle_02.png',
         '../img2/2_character/1_idle/idle/biker_idle_03.png',
         '../img2/2_character/1_idle/idle/biker_idle_04.png',
     ]
+    IMAGES_WALK = [
+        '../img2/2_character/2_walk/biker_walk_01.png',
+        '../img2/2_character/2_walk/biker_walk_02.png',
+        '../img2/2_character/2_walk/biker_walk_03.png',
+        '../img2/2_character/2_walk/biker_walk_04.png',
+        '../img2/2_character/2_walk/biker_walk_05.png',
+        '../img2/2_character/2_walk/biker_walk_06.png',
+    ]
     world;
 
     constructor() {
-        super().loadImage('../img2/2_character/1_idle/idle/biker_idle_01.png')
-        this.loadImages(this.IMAGES_IDLE);
+        super().loadImage('../img2/2_character/2_walk/biker_walk_01.png')
+        this.loadImages(this.IMAGES_WALK);
         this.animate();
     }
 
     animate() {
         setInterval(() => {
-            let i = this.currentImage % this.IMAGES_IDLE.length; // Index wird nie so groß wie die Anzahl der Bilder im Array, also Array.Länge -1
-            // durch Modulo steht hier i = 0, 1, 2, 3, 0, 1, 2, 3, 0,...
-            let path = this.IMAGES_IDLE[i];
-            this.img = this.imageCache[path];
-            this.currentImage++
+            if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+
+                let i = this.currentImage % this.IMAGES_WALK.length; // Index wird nie so groß wie die Anzahl der Bilder im Array, also Array.Länge -1
+                // let i = this.currentImage % this.IMAGES_IDLE.length; // Index wird nie so groß wie die Anzahl der Bilder im Array, also Array.Länge -1
+                // durch Modulo steht hier i = 0, 1, 2, 3, 0, 1, 2, 3, 0,...
+                let path = this.IMAGES_WALK[i];
+                this.img = this.imageCache[path];
+                this.currentImage++
+            }
         }, 430);
     }
 
