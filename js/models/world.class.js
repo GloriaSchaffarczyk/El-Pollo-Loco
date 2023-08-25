b82df3421fa923a15b9cc07e537519fdd75d2a7b
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2023-08-25 23:45:55 +0200

    added background images

--- js/models/world.class.js
+++ js/models/world.class.js
@@ -15,6 +15,10 @@ class World {
         new Cloud()
     ];
     backgroundObjects = [
+        new BackgroundObject('../img2/5_background/layers/cemetary_background_night_02.png', -1706, 0),
+        new BackgroundObject('../img2/5_background/layers/3_third_layer/cemetary_thirdlayer_night_02.png', -1706, 0),
+        new BackgroundObject('../img2/5_background/layers/2_second_layer/cemetary_secondlayer_night_02.png', -1706, 0),
+        new BackgroundObject('../img2/5_background/layers/1_first_layer/cemetary_firstlayer_night_02.png', -1706, 0),
         new BackgroundObject('../img2/5_background/layers/cemetary_background_night_02.png', -853, 0),
         new BackgroundObject('../img2/5_background/layers/3_third_layer/cemetary_thirdlayer_night_02.png', -853, 0),
         new BackgroundObject('../img2/5_background/layers/2_second_layer/cemetary_secondlayer_night_02.png', -853, 0),
