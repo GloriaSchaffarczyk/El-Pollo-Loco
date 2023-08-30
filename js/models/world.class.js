b766fe767e4194c63dcb2666069a2de7523df50e
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2023-08-31 00:25:37 +0200

    reduced variables to LEVEL1.enemies etc.

--- js/models/world.class.js
+++ js/models/world.class.js
@@ -1,10 +1,7 @@
 class World {
-    character = new Character(); 
+    character = new Character();
     // wenn wir auf die Variablen aus dieser Klasse zugreifen wollen, brauchen wir "this"
-    zombies = LEVEL1.zombies;
-    monsters = LEVEL1.monsters;
-    clouds = LEVEL1.clouds;
-    backgroundObjects = LEVEL1.backgroundObjects;
+    level = LEVEL1;
     canvas;
     ctx;
     keyboard;
@@ -26,35 +23,35 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.translate(this.camera_x, 0); // Kamera wird nach links verschoben
-        this.addObjectsToMap(this.backgroundObjects);
-        this.addObjectsToMap(this.clouds);
-        this.addToMap(this.character); 
-        this.addObjectsToMap(this.zombies);
-        this.addObjectsToMap(this.monsters); // Monster werden eingefügt
+        this.addObjectsToMap(this.level.backgroundObjects);
+        this.addObjectsToMap(this.level.clouds);
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.level.zombies);
+        this.addObjectsToMap(this.level.monsters); // Monster werden eingefügt
 
         this.ctx.translate(-this.camera_x, 0); // Kamera wird zurückgesetzt
 
-        requestAnimationFrame(() => this.draw()); 
+        requestAnimationFrame(() => this.draw());
     }
 
     addObjectsToMap(objects) {
         objects.forEach(object => {
             this.addToMap(object);
-        }); 
+        });
     }
 
     addToMap(movableObject) {
-        if(movableObject.otherDirection) { 
+        if (movableObject.otherDirection) {
             this.ctx.save();
-            this.ctx.translate(movableObject.width, 0); 
-            this.ctx.scale(-1, 1); 
+            this.ctx.translate(movableObject.width, 0);
+            this.ctx.scale(-1, 1);
             movableObject.x = movableObject.x * -1;
         }
-        this.ctx.drawImage(movableObject.img, movableObject.x, movableObject.y, movableObject.width, movableObject.height); 
-        if(movableObject.otherDirection) { 
+        this.ctx.drawImage(movableObject.img, movableObject.x, movableObject.y, movableObject.width, movableObject.height);
+        if (movableObject.otherDirection) {
             movableObject.x = movableObject.x * -1;
             this.ctx.restore();
         }
     }
-    
+
 }
