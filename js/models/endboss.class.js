5917f2ca613df3d2d06a6fa1befb97fd4305bbff
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2023-09-06 23:34:57 +0200

    added Endboss

--- js/models/endboss.class.js
+++ js/models/endboss.class.js
@@ -1,3 +1,42 @@
 class Endboss extends MovableObject {
-    
+    y = 200;
+    height = 200;
+    width = 200;
+    ENDBOSS_IMAGES_WALKING = [
+        '../img2/4_boss/1_walk/boss_walk_right_01.png',
+        '../img2/4_boss/1_walk/boss_walk_right_02.png',
+        '../img2/4_boss/1_walk/boss_walk_right_03.png',
+        '../img2/4_boss/1_walk/boss_walk_right_04.png',
+        '../img2/4_boss/2_alert/boss_alert_01.png',
+        '../img2/4_boss/2_alert/boss_alert_02.png',
+        '../img2/4_boss/2_alert/boss_alert_03.png',
+        '../img2/4_boss/2_alert/boss_alert_04.png',
+        '../img2/4_boss/2_alert/boss_alert_05.png',
+        '../img2/4_boss/1_walk/boss_walk_left_01.png',
+        '../img2/4_boss/1_walk/boss_walk_left_02.png',
+        '../img2/4_boss/1_walk/boss_walk_left_03.png',
+        '../img2/4_boss/1_walk/boss_walk_left_04.png',
+        '../img2/4_boss/2_alert/boss_alert_05.png',
+        '../img2/4_boss/2_alert/boss_alert_04.png',
+        '../img2/4_boss/2_alert/boss_alert_03.png',
+        '../img2/4_boss/2_alert/boss_alert_02.png',
+        '../img2/4_boss/2_alert/boss_alert_01.png',
+    ];
+
+    constructor() {
+        super().loadImage('../img2/4_boss/1_walk/boss_walk_right_01.png');
+        this.loadImages(this.ENDBOSS_IMAGES_WALKING);
+        this.x = 2000;
+        this.speed = 0.25;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.ENDBOSS_IMAGES_WALKING.length;
+            let path = this.ENDBOSS_IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++
+        }, 200);
+    }
 }
