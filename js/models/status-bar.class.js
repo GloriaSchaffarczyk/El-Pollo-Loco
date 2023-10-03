0c4ddc2e3fcdb6cb82952106f0fb1e791d17f913
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2023-10-03 21:59:21 +0200

    added statusbar health and added constructor in class status-bar

--- js/models/status-bar.class.js
+++ js/models/status-bar.class.js
@@ -1,3 +1,38 @@
 class StatusBar {
-    
+    IMAGES_HEALTH = [
+        '../img2/7_statusbars/1_statusbar/2_statusbar_health/statusbar-health_01.png',
+        '../img2/7_statusbars/1_statusbar/2_statusbar_health/statusbar-health_02.png',
+        '../img2/7_statusbars/1_statusbar/2_statusbar_health/statusbar-health_03.png',
+        '../img2/7_statusbars/1_statusbar/2_statusbar_health/statusbar-health_04.png',
+        '../img2/7_statusbars/1_statusbar/2_statusbar_health/statusbar-health_05.png',
+        '../img2/7_statusbars/1_statusbar/2_statusbar_health/statusbar-health_06.png',
+    ];
+
+    percentage = 100;
+
+    constructor() {
+        this.loadImages(this.IMAGES_HEALTH);
+    }
+
+    setPercenage(percentage) {
+        this.percentage = percentage; // Zahl zwischen 0 und 5 ermitteln
+        let path = this.IMAGES_HEALTH[this.resolveImageIndex()]; // Holt sich den Pfad also Index an der richtigen Stelle
+        this.img = this.imageCache[path]; // holt sich das Bild an der jeweiligen Stelle
+    }
+
+    resolveImageIndex() {
+        if (this.percentage == 100) {
+            return 5;
+        } else if (this.percentage > 80) {
+            return 4;
+        } else if (this.percentage > 60) {
+            return 3;
+        } else if (this.percentage > 40) {
+            return 2;
+        } else if (this.percentage > 20) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
