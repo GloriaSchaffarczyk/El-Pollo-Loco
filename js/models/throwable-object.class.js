2d5b4bef34deab187f5d15e9a72505278dbce859
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2024-01-23 23:08:02 +0100

    throw works now, bomb exposion and character throw animation are still missing

--- js/models/throwable-object.class.js
+++ js/models/throwable-object.class.js
@@ -17,7 +17,6 @@ class ThrowableObject extends MovableObject {
         'img2/6_bombs/bomb_explosion/bomb-explosion_08.png',
     ];
     hitEnemy = false;
-    speedX = 0;
 
     constructor(x, y, direction) {
         super().loadImage('img2/6_bombs/bomb_rotation/bomb-rotation_01.png',);
@@ -30,22 +29,16 @@ class ThrowableObject extends MovableObject {
         this.throw();
     }
 
-    // wenn sich der Character umdreht, muss die Flasche in die entgegengesetze Richtung fliegen, als muss += 10 angepasst werden
+
     throw() {
         this.speedY = 30; // Anfangsgeschwindigkeit nach oben
-        this.speedX = this.direction === 'right' ? 10 : -10; 
-        
+        this.speedX = this.direction === 'right' ? 10 : -10; // Setzen der horizontalen Geschwindigkeit basierend auf der Richtung
+
+        this.applyGravity();
         setInterval(() => {
-            if (this.hitEnemy) {
-                this.playAnimation(this.IMAGES_BOMBEXPLOSION);
-            } else {
-                this.x += 10;
-                this.y = y;
-                this.speedY = 30;
-                this.x += this.speedX; // Horizontale Bewegung
-                this.applyGravity();
-                this.playAnimation(this.IMAGES_BOMBROTATION);
-            }
+            this.x += this.speedX; // Horizontale Bewegung aktualisieren
+            this.playAnimation(this.IMAGES_BOMBROTATION); // Rotation der Bombe animieren
         }, 25);
-    }       
+    }
+
 }
