6a7147d5f7255ebb94063e3e2abe468d208e122a
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2024-03-16 11:47:43 +0100

    gac

--- js/models/drawable-object.class.js
+++ js/models/drawable-object.class.js
@@ -25,31 +25,31 @@ class DrawableObject {
         }
     }
 
-    // drawFrame(ctx) {
-    //     if (this instanceof Character || this instanceof Zombie || this instanceof Monster || this instanceof Endboss || this instanceof Candy || this instanceof Bombs) {
-    //         ctx.beginPath();
-    //         // ctx.lineWidth = '5';
-    //         // ctx.strokeStyle = 'red';
-    //         ctx.rect(this.x, this.y, this.width, this.height);
-    //         // ctx.stroke();
-    //     }
-    // }
+    drawFrame(ctx) {
+        if (this instanceof Character || this instanceof Zombie || this instanceof Monster || this instanceof Endboss || this instanceof Candy || this instanceof Bombs) {
+            ctx.beginPath();
+            ctx.lineWidth = '5';
+            ctx.strokeStyle = 'red';
+            ctx.rect(this.x, this.y, this.width, this.height);
+            ctx.stroke();
+        }
+    }
 
-    // drawFrameBlue(ctx) {
-    //     if (this instanceof Character || this instanceof Zombie || this instanceof Monster || this instanceof Endboss || this instanceof Candy || this instanceof Bombs) {
-    //         ctx.beginPath();
-    //         // ctx.lineWidth = '5';
-    //         // ctx.strokeStyle = 'blue'; // Setze die Farbe des Rahmens auf Blau
-    //         // Berechne die Position und Größe des Rahmens basierend auf den Objekteigenschaften und deren Offset
-    //         ctx.rect(
-    //             this.x + (this.offset ? this.offset.left : 0),
-    //             this.y + (this.offset ? this.offset.top : 0),
-    //             this.width - (this.offset ? this.offset.left + this.offset.right : 0),
-    //             this.height - (this.offset ? this.offset.top + this.offset.bottom : 0)
-    //         );
-    //         // ctx.stroke();
-    //     }
-    // }    
+    drawFrameBlue(ctx) {
+        if (this instanceof Character || this instanceof Zombie || this instanceof Monster || this instanceof Endboss || this instanceof Candy || this instanceof Bombs) {
+            ctx.beginPath();
+            ctx.lineWidth = '5';
+            ctx.strokeStyle = 'blue'; // Setze die Farbe des Rahmens auf Blau
+            Berechne die Position und Größe des Rahmens basierend auf den Objekteigenschaften und deren Offset
+            ctx.rect(
+                this.x + (this.offset ? this.offset.left : 0),
+                this.y + (this.offset ? this.offset.top : 0),
+                this.width - (this.offset ? this.offset.left + this.offset.right : 0),
+                this.height - (this.offset ? this.offset.top + this.offset.bottom : 0)
+            );
+            ctx.stroke();
+        }
+    }    
 
     /**
     * 
