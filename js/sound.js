3e0eea6da34d3ba31780a5347810a219e6ce0c4c
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2024-03-30 23:58:24 +0100

    added sound.js and added endscreenSound

--- js/sound.js
+++ js/sound.js
@@ -5,3 +5,11 @@ const startscreenSound = new Audio({
     loadLimit: 1,
     volume: 0.2,
 });
+
+const endscreenSound = new Audio({
+    tracks: ['audio/475300__adnova__bell-cave.wav'],
+    loop: false,
+    singleMode: true,
+    loadLimit: 1,
+    volume: 0.2,
+});
