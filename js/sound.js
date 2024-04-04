dd0fd8f1dd3db74dd46f069922b8da1c0ad2fb3f
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2024-04-04 23:32:58 +0200

    sound.js: added new sound for endscreen defeat

--- js/sound.js
+++ js/sound.js
@@ -1,4 +1,4 @@
-const startscreenSound = new Audio({
+const startscreenSound = new Audio1({
     tracks: ['audio/475300__adnova__bell-cave.wav'],
     loop: false,
     singleMode: true,
@@ -6,8 +6,16 @@ const startscreenSound = new Audio({
     volume: 0.2,
 });
 
-const endscreenSound = new Audio({
+const endscreenSoundWin = new Audio2({
     tracks: ['audio/475300__adnova__bell-cave.wav'],
+    loop: false,
+    singleMode: true,
+    loadLimit: 1,
+    volume: 0.2,
+});
+
+const endscreenSoundDefeat = new Audio3({
+    tracks: ['audio/342895__kickhat__day-of-defeat.mp3'],
     loop: false,
     singleMode: true,
     loadLimit: 1,
