0e5a6a5d329f215b446f6812e69b416bddc9c05b
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2024-04-08 23:21:08 +0200

    sound.js: added new sounds for character

--- js/sound.js
+++ js/sound.js
@@ -21,3 +21,43 @@ const endscreenSoundDefeat = new Audio3({
     loadLimit: 1,
     volume: 0.2,
 });
+
+const walking_sound = new Audio4({
+    tracks: ['audio/659370__matrixxx__retro-footsteps.wav'],
+    loop: false,
+    singleMode: true,
+    loadLimit: 1,
+    volume: 0.2,
+});
+
+const jumping_sound = new Audio5({
+    tracks: ['audio /678839__cartchaos__jump.wav'],
+    loop: false,
+    singleMode: true,
+    loadLimit: 1,
+    volume: 0.2,
+});
+
+const background_music = new Audio6({
+    tracks: ['audio /363164__adnova__spooker.wav'],
+    loop: false,
+    singleMode: true,
+    loadLimit: 1,
+    volume: 0.2,
+});
+
+const dying_sound = new Audio7({
+    tracks: ['audio/163442__under7dude__man-dying.wav'],
+    loop: false,
+    singleMode: true,
+    loadLimit: 1,
+    volume: 0.2,
+});
+
+const hurt_sound = new Audio8({
+    tracks: ['audio/486943__matrixxx__human-aah.wav'],
+    loop: false,
+    singleMode: true,
+    loadLimit: 1,
+    volume: 0.2,
+});
