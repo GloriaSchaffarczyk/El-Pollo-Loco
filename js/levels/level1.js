a170024871be004c62f57279ac2ad4f028c99a86
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2024-04-09 22:32:11 +0200

    added JSDoc-comment to level1.js

--- js/levels/level1.js
+++ js/levels/level1.js
@@ -1,5 +1,9 @@
 let LEVEL1;
 
+/**
+ * Initializes the level by creating a new `Level` instance
+ * and assigning it to the global variable `LEVEL1`.
+ */
 function initLevel() {
     LEVEL1 = new Level();
 }
