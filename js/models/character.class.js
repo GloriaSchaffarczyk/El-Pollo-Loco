33fbcd7289bb4d1f506a194c3e5fc7bc949c2416
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2024-04-22 23:17:21 +0200

    character.class.js: tried fixing bomb throwing problem

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -100,13 +100,13 @@ class Character extends MovableObject {
         'img2/2_character/6_throw/biker_throw_05.png',
         'img2/2_character/6_throw/biker_throw_06.png',
     ];
-    ANIMATION_SPEED_IDLE = 40;
-    ANIMATION_SPEED_LONG_IDLE = 40;
-    ANIMATION_SPEED_WALK = 50;
-    ANIMATION_SPEED_JUMP = 55;
-    ANIMATION_SPEED_HURT = 50;
-    ANIMATION_SPEED_DEAD = 40;
-    ANIMATION_SPEED_THROWINGBOMBS = 45;
+    ANIMATION_SPEED_IDLE = 250;
+    ANIMATION_SPEED_LONG_IDLE = 80;
+    ANIMATION_SPEED_WALK = 120;
+    ANIMATION_SPEED_JUMP = 120;
+    ANIMATION_SPEED_HURT = 250;
+    ANIMATION_SPEED_DEAD = 200;
+    ANIMATION_SPEED_THROWINGBOMBS = 50;
     world;
     animationInterval = null;
     hasDied = false;
@@ -129,6 +129,7 @@ class Character extends MovableObject {
         this.loadImages(this.IMAGES_HURT);
         this.loadImages(this.IMAGES_DEAD);
         this.loadImages(this.IMAGES_THROWINGBOMBS);
+        this.currentAnimationSpeed = this.ANIMATION_SPEED_IDLE;
         this.animate();
         this.applyGravity();
         // this.lastAnimationState = null;
@@ -142,30 +143,42 @@ class Character extends MovableObject {
         this.animationInterval = setInterval(() => {
             this.updateState();
             this.updateAnimation();
-        }, 100); // 60 mal pro Sekunde aktualisieren
+        }, this.currentAnimationSpeed); // Verwende die aktuelle Animationsgeschwindigkeit
     }
-    
+
+    /**
+     * Updates the state of the character based on user input and character conditions.
+     * This method handles keyboard inputs for moving left, right, jumping, and sets idle time.
+     * It also adjusts the camera's x-position based on the character's position.
+     */
     updateState() {
         if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
             this.moveRight();
             this.otherDirection = false;
             sounds.walkingSound.play(5);
             this.idleTime = 0;
-        } else if (this.world.keyboard.LEFT && this.x > -650) {
+        } if (this.world.keyboard.LEFT && this.x > -650) {
             this.moveLeft();
             this.otherDirection = true;
             sounds.walkingSound.play(5);
             this.idleTime = 0;
-        } else if (this.world.keyboard.SPACE && !this.isAboveGround()) {
+        } if (this.world.keyboard.SPACE && !this.isAboveGround()) {
             this.jump();
             sounds.jumpingSound.play();
             this.idleTime = 0;
-        } else if (!(this.world.keyboard.RIGHT || this.world.keyboard.LEFT || this.world.keyboard.SPACE)) {
+        } if (this.world.keyboard.W && !this.isThrowingBomb) {
+            this.throwBomb();
+            this.world.keyboard.W = false; 
+        } if (!(this.world.keyboard.RIGHT || this.world.keyboard.LEFT || this.world.keyboard.SPACE)) {
             this.idleTime += 1000 / 60;
         }
         this.world.camera_x = -this.x + 100;
     }
-    
+
+    /**
+    * Updates the animation of the character based on the current state and active conditions.
+    * This method selects the appropriate animation based on character actions like walking, jumping, or taking damage.
+    */
     updateAnimation() {
         if (this.isDead()) {
             if (!this.hasDied) {
@@ -173,40 +186,61 @@ class Character extends MovableObject {
                 this.hasDied = true;
                 endGame(false);
             }
-        } else if (this.isThrowingBomb) {
-            this.playAnimation(this.IMAGES_THROWINGBOMBS);
         } else if (this.isHurt()) {
             this.playAnimation(this.IMAGES_HURT);
+            this.updateIntervalSpeed(this.ANIMATION_SPEED_HURT);
         } else if (this.isAboveGround() && this.canDoubleJump) {
             this.playAnimation(this.IMAGES_DOUBLE_JUMP);
+            this.updateIntervalSpeed(this.ANIMATION_SPEED_JUMP);
         } else if (this.isAboveGround()) {
             this.playAnimation(this.IMAGES_JUMP);
+            this.updateIntervalSpeed(this.ANIMATION_SPEED_JUMP);
         } else if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
             this.playAnimation(this.IMAGES_WALK);
+            this.updateIntervalSpeed(this.ANIMATION_SPEED_WALK);
         } else if (this.idleTime > 5000) {
             this.playAnimation(this.IMAGES_LONG_IDLE);
+            this.updateIntervalSpeed(this.ANIMATION_SPEED_LONG_IDLE);
         } else {
             this.playAnimation(this.IMAGES_IDLE);
+            this.updateIntervalSpeed(this.ANIMATION_SPEED_IDLE);
         }
     }
-    
+
+    /**
+    * Adjusts the speed of the animation interval.
+    * This method updates the interval timing based on the new speed required for the current animation state.
+    * @param {number} newSpeed - The new speed for the animation interval in milliseconds.
+    */
+    updateIntervalSpeed(newSpeed) {
+        if (this.currentAnimationSpeed !== newSpeed) {
+            this.currentAnimationSpeed = newSpeed;
+            clearInterval(this.animationInterval);
+            this.animationInterval = setInterval(() => {
+                this.updateState();
+                this.updateAnimation();
+            }, this.currentAnimationSpeed);
+        }
+    }
 
     /**
      * Handles the bomb throwing mechanism for the character, managing the animation and gameplay effects.
      */
     throwBomb() {
-        this.currentImage = 0;
-        this.isThrowingBomb = true;
-        const animationInterval = setInterval(() => {
-            this.playAnimation(this.IMAGES_THROWINGBOMBS);
-            if (this.currentImage >= this.IMAGES_THROWINGBOMBS.length) {
-                clearInterval(animationInterval);
-                this.isThrowingBomb = false;
-                this.currentImage = 0;
-                this.reducingBombs();
-            }
-        }, this.ANIMATION_SPEED_THROWINGBOMBS);
-    }
+        if (this.ownedBombs > 0 && !this.isThrowingBomb) {
+            this.isThrowingBomb = true;
+            this.currentImage = 0;
+            const animationInterval = setInterval(() => {
+                this.playAnimation(this.IMAGES_THROWINGBOMBS);
+                if (this.currentImage >= this.IMAGES_THROWINGBOMBS.length) {
+                    clearInterval(animationInterval);
+                    this.isThrowingBomb = false;
+                    this.currentImage = 0;
+                    this.reducingBombs();
+                }
+            }, 100);
+        }
+    }    
 
     /**
      * Handles the animation sequence for the dying animation of the character.
